9d55eb9473980df1fdd30305d758e24eb6e9c56d
Shah Nirmal Dipakkumar <nirmalshah20519@gmail.com>
2024-07-11 03:06:07 +0530

    change 4

--- frontend/src/Services/news.serv.js
+++ frontend/src/Services/news.serv.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const endpoint = "https://35.244.40.220:8080";
+const endpoint = "http://35.244.40.220:8080";
 // const endpoint = "https://nirmalshah20519.pythonanywhere.com";
 
 export const getTodayNews = async () => {
